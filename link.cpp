f40af8111131eecee4f2e8d344e03380c616420a
dway123 <huangdarwin2@gmail.com>
2016-02-29 18:07:18 -0500

    Let it flowwww
    added some flows ayyyyyy

--- link.cpp
+++ link.cpp
@@ -7,19 +7,17 @@ using namespace std;
 
 
   // The link constructor initializes the link with set information of data and destination.
-  link::link(int initTransDir, int currentT, int maxT, Router* src, Router* dst){
-	int transmitDirection = initTransDir;
-	int currentThroughput = currentT;
-	int maxThroughput = maxT; 
-	Router* source = src;
-	Router* destination = dst; 
+  link::link(int f, int maxT, Router* src, Router* dst){
+	flow = f;
+	maxThroughput = maxT; 
+	source = src;
+	destination = dst; 
   }  
   
   // Displays the packet for testing purposes
   link::display(){
 	cout << "Link display:" << endl;
-	cout << "The transmit direction is:\t" << transmitDirection << endl;
-	cout << "The current throughput is:\t" << currentThroughput << endl;
+	cout << "Flow:\t" << flow << endl;
 	cout << "The maximum throughput is:\t" << maxThroughput << endl;
 	cout << "The link goes from " << *src << " to " << *dst << endl;
 	cout << "The link's buffer going into the router contains the following packets: " << endl;
--- link.h
+++ link.h
@@ -9,15 +9,14 @@ class link {
  public:
 
   // The link constructor initializes the link with set information of data and destination.
-  link(int initTransDir, int currentT, int maxT, Router* src, Router* dst);
+  link(int f, int maxT, Router* src, Router* dst);
   
   // Displays the link for testing purposes
   display();
 
  private:
   vector<packet> buffer();
-  int transmitDirection = -1; //-1 for not set, 0 for reverse, 1 for forward
-  int currentThroughput = -1; //the amount of packets going through
+  int flow = 0; //negative for backwards, positive for forwards, magnitude indicates current throughput
   int maxThroughput = -1; // maximum data that can go through the packet
   Router* source = nullptr; //start of link
   Router* destination = nullptr; //end of link
