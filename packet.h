5839c866df1f47696c010d30cc9c78dfc82b3bb7
dway123 <huangdarwin2@gmail.com>
2016-02-29 13:32:40 -0500

    Added some bs packet code

---
+++ packet.h
@@ -0,0 +1,25 @@
+//ECE303 Project
+//Original Author: Darwin Huang
+
+#ifndef _PACKET_H
+#define _PACKET_H
+
+class packet {
+
+ public:
+
+  // The packet constructor initializes the packet with set information of data and destination. Must also find the time...
+  packet(int dat, Router* src, Router* dst);
+  
+  // Displays the packet for testing purposes
+  display();
+
+ private:
+  int data = -1;
+  int timestamp = -1; // Time of birth for the packet
+  Router* source = nullptr; // Packet birthplace
+  Router* destination = nullptr; // Destiny determines that the packet must go here to rest
+
+};
+
+#endif //_PACKET_H
