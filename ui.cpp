f23c175aee31e596d31bc5d3773b6ebd7225c641
dway123 <huangdarwin2@gmail.com>
2016-04-06 01:15:49 -0400

    FATHER TIME HAS ARRIVED
    added time to the shitttttttttttttt
    
    (the heap and time management functions have been implemented)

--- ui.cpp
+++ ui.cpp
@@ -1,7 +1,14 @@
 #include <cstdlib>
 #include <iostream>
 #include <string>
+#include <queue> //for std::priority_queue
 using namespace std;
+
+//GLOBAL VARIABLES for time management
+std::priority_queue<string> q;
+int time = 0;
+string log = "";
+vector<link> linkVector;
 
 // Display an ASCII View of how the network should look
 void printNetwork()
@@ -33,9 +40,53 @@ void SimulateNetwork(){
 	
 }
 
+//Expects an event in the form of a string with standardized format, as well as time to elapse for the event
+//Adds the "added time" and "time to execute" onto the start of string for use as the comparable to order events
+//An example of a working format for e is LINK_0_SEND_PACKET or HOST_1_RECEIVE_PACKET
+void pushEvent(string e, int elapseTime){
+	int currentTime = time;
+	int executeTime = time + elapseTime;
+	string event = (string)executeTime + "," + (string)currentTime + "," + e; //csv for simplicity
+	q.push(event);
+}
+
+//Pops an event from the event queue and executes the appropriate command, as contained in e and assuming correct format of e
+//Pushes data onto a log for output
+void popEvent(){
+	if(q.empty()){
+		cout << "ERROR: The event queue is empty! Cannot pop event!"
+		return;
+	}
+	string event = q.pop();
+	
+	//Extract the original event message from the expanded event message
+	int find = event.find_last_of(","); //right after the last comma lies the original event message
+	string e = event.substr(lastComma+1);
+	
+	//Extract the original event message's meaning
+	find = e.find("_");
+	string objectType = e.substr(0,find);
+	
+	e = e.substr(find+1); //gets the rest of the message
+	find = e.find("_"); 
+	string objectIndex = e.substr(0,find);
+	
+	string function = e.substr(find+1);
+	
+	//Execute the event in the event e that was initially input into pushEvent
+	if(objectType == "LINK"){
+		int index = stoi(objectIndex)
+		if(function == "RECIEVE_PACKET"){
+			link[index].recievePacket();	
+		}
+	}
+	
+	log += endl + event; //add the event to the log
+}
+
 int main(int argc, char *argv[])
 {
-	cout<<"Welcome to the Network Simulator"<<endl;
+	cout<<"Welcome to the Network Simulator!"<<endl;
 	string s = "s";
 	cout<<"Enter the name of the first host:"<<endl;
 	string h1;
