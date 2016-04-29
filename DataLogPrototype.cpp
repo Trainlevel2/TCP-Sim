f1d049b5d0522cd6c7c319d9bb4b45fca764f2a3
dway123 <huangdarwin2@gmail.com>
2016-04-29 04:52:50 -0400

    Some more fun fun changes
    mmmm so fun
    
    Mark halp tmrw plzz

--- DataLogPrototype.cpp
+++ DataLogPrototype.cpp
@@ -11,7 +11,7 @@ string bufferLog = "bufferLog \nlink index, time(s), buffer occupancy";
 string cwndLog = "cwndLog \nflow index, time(s) , cwnd";
 
 //event driven
-string packetLossLog = "packetLossLog \nflow index, time(s), packets lost"; //TODO: Can we figure out how many packets were lost?
+string packetLossLog = "packetLossLog \nflow index, time(s), packets lost"; //TODO ASK: Can we figure out how many packets were lost?
 string flowRateLog = "flowRateLog \nflow index, time(s), flow rate(dataSize/ms)";
 string linkRateLog = "linkRateLog \nlink index, time(s), link rate (dataSize/ms)";
 string packetDelayLog = "packetDelayLog \nflow index, time(s), packetDelay(ms)";
@@ -20,8 +20,10 @@ string packetDelayLog = "packetDelayLog \nflow index, time(s), packetDelay(ms)";
 //EXTRA INITIALIZATION FOR LOG CALCULATIONS
 //Rate for a given flow/link, from a time t to time t+dt, is the summation of the rates of all packets in the flow/link at that time interval.
 int dt = 100; //milliseconds per sample. Increase for gradation, decrease for speed. If this is infinity, you get avg rate.
-vector<vector<double> > flowRate(flowVector.size()); //rows are flows and columns are times. TODO: initialize flowRate's column dimension to flowVector.size() (check vector constructor syntax)
-vector<vector<double> > linkRate(linkVector.size()); //rows are links and columns are times. TODO: initialize linkRate's column dimension to linkVector.size() (check vector constructor syntax)
+vector<vector<double> > flowRate(); //rows are flows and columns are times.
+vector<vector<double> > linkRate(); //rows are links and columns are times.
+flowRate.resize(flowVector.size());
+linkRate.resize(linkVector.size());
 int endTime;
 int startTime;
 double packetRate;
@@ -32,13 +34,15 @@ POPEVENT(){
 	//FLOW AND LINK RATE CALCULATIONS
 	if(event.find("PACKET") > 0){
 		int endTime = t; //remove "endTime"/"startTime" for actual code. It's just for readability now
-		int startTime = startTime; //TODO: get startTime
-		double packetRate = packet.dataSize / (endTime - startTime); //TODO: get packet's dataSize
+		int startTime = startTime; //TODO DARWIN: get startTime
+		double packetRate = packet.dataSize / (endTime - startTime); //TODO ASK: get packet's dataSize
 		
 		//check for if flowRate/linkRate time dimension is large enough. If not, increase it until it is.
-		while(flowRate.size() < endTime/dt){
+		while(flowRate.at(0).size() < endTime/dt){
 			for(int i = 0; i < flowVector.size(); i++){
 				flowRate.at(i).pushBack(0);
+			}
+			for(int i = 0; i < linkVector.size(); i++){
 				linkRate.at(i).pushBack(0);
 			}
 		}
@@ -61,19 +65,19 @@ POPEVENT(){
 		//PACKETLOSSLOG
 		//example event -> FLOW_0_TIMEOUT_5
 		if(event.find("TIMEOUT") > 0){
-			packetLossLog += "\n" + flowIndex + "," + t/1000 + "," + amtPacketsLost; //TODO: Extract the flowIndex from the event so that the .csv will be easier to parse
-			//TODO: get amtPacketsLost how?
+			packetLossLog += "\n" + flowIndex + "," + t/1000 + "," + amtPacketsLost; //TODO DARWIN: Extract the flowIndex from the event so that the .csv will be easier to parse
+			//TODO ASK: get amtPacketsLost how?
 		}
 		
 		//PACKETDELAYLOG
-		//TODO: create packet sent event for packets sent across a flow for this and flowRateLog
-		else if(event.find("TRANSMIT_PACKET" > 0){ //TODO: check syntax after FLOW_2_TRANSMIT_PACKET event exists/works
+		//TODO ASK: create packet sent event for packets sent across a flow for this and flowRateLog
+		else if(event.find("TRANSMIT_PACKET" > 0){ //TODO WAIT: check syntax after FLOW_2_TRANSMIT_PACKET event exists/works
 			packetDelaylog += "\n" + flowIndex +  "," + t/1000  + "," + (endTime-startTime);
 		}	
 	}
 	
 	//PERIODICALLY COLLECT:
-	if(event.find("DATA_ACQUISITION")>0){ //TODO: create data acquisition events that spawn other data acquisition events
+	if(event.find("DATA_ACQUISITION")>0){ //TODO ASK: create data acquisition events that spawn other data acquisition events
 		//BUFFER SIZE
 		for(int i = 0; i < (int)linkVector.size(); i++){
 			//bufferLog += "\n" + i + "," + t / 1000 + "," + linkVector[i].getBufferSize();
